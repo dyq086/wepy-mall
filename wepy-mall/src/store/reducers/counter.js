2db7a018959220e5282c1fa89e6f089a55bf9d14
toyweiZH <g1e2t3@qq.com>
2018-08-24 16:10:33 +0800

    wepy-mall/src/utils/util.js  依据https://eslint.org/docs/2.0.0/rules修改

---
+++ wepy-mall/src/store/reducers/counter.js
@@ -0,0 +1,26 @@
+import { handleActions } from 'redux-actions'
+import { INCREMENT, DECREMENT, ASYNC_INCREMENT } from '../types/counter'
+
+export default handleActions({
+  [INCREMENT] (state) {
+    return {
+      ...state,
+      num: state.num + 1
+    }
+  },
+  [DECREMENT] (state) {
+    return {
+      ...state,
+      num: state.num - 1
+    }
+  },
+  [ASYNC_INCREMENT] (state, action) {
+    return {
+      ...state,
+      asyncNum: state.asyncNum + action.payload
+    }
+  }
+}, {
+  num: 0,
+  asyncNum: 0
+})
---
+++ wepy-mall/wepy.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  build: {
+    web: {
+      htmlTemplate: path.join('src', 'index.template.html'),
+      htmlOutput: path.join('web', 'index.html'),
+      jsOutput: path.join('web', 'index.js')
+    }
+  },
+  resolve: {
+    alias: {
+      counter: path.join(__dirname, 'src/components/counter'),
+      '@': path.join(__dirname, 'src')
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: prod
+    },
+    /*sass: {
+      outputStyle: 'compressed'
+    },*/
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-object-rest-spread',
+        'transform-export-extensions',
+      ]
+    }
+  },
+  plugins: {
+  },
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
+if (prod) {
+
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
