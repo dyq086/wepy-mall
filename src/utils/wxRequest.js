84a21bdfd4277244cbe8e62eeed4f9e59c5c7956
toywei <g1e2t3@qq.com>
2018-09-06 08:50:08 +0800

    完善wxRequest()请求参数扩展

--- src/utils/wxRequest.js
+++ src/utils/wxRequest.js
@@ -1,28 +1,44 @@
-import wepy from 'wepy';
-import util from './util';
-import md5 from './md5';
+import wepy from 'wepy'
+import util from './util'
+import md5 from './md5'
 import tip from './tip'
 
-const API_SECRET_KEY = 'www.mall.cycle.com'
-const TIMESTAMP = util.getCurrentTime()
-const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
-
-const wxRequest = async(params = {}, url) => {
-    tip.loading();
-    let data = params.query || {};
-    data.sign = SIGN;
-    data.time = TIMESTAMP;
-    let res = await wepy.request({
-        url: url,
-        method: params.method || 'GET',
-        data: data,
-        header: { 'Content-Type': 'application/json' },
-    });
-    tip.loaded();
-    return res;
-};
-
+const appendInfo = () => {
+  const API_SECRET_KEY = 'https://github.com/dyq086/wepy-mall/tree/master/src'
+  const TIMESTAMP = util.getCurrentTime()
+  const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
+  const MORE = 'more......'
+  return {
+    'API_SECRET_KEY': API_SECRET_KEY,
+    'TIMESTAMP': TIMESTAMP,
+    'SIGN': SIGN,
+    'MORE': MORE
+  }
+}
+const wxRequest = async (params = {}, url) => {
+  console.log('wxRequest')
+  tip.loading()
+  let data = params.query || {}
+  const header = params.header || {
+    'Content-Type': 'application/json'
+  }
+  const isAppend = params.isAppend || false
+  if (isAppend) {
+    const a = appendInfo()
+    for (let k in a) {
+      eval('data.' + k + '= a.' + k)
+    }
+  }
+  const res = await wepy.request({
+    url: url,
+    method: params.method || 'GET',
+    data: data,
+    header: header
+  })
+  tip.loaded()
+  return res
+}
 
 module.exports = {
-    wxRequest
+  wxRequest
 }
